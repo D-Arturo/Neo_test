21ae375b491a04a971f9ab699e5ae1c325d0df7e
D-Arturo <73534635+D-Arturo@users.noreply.github.com>
2021-04-25 22:36:45 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -22,18 +22,126 @@ include "templates/header.php";
         </a><!-- .shop__button -->
     </div><!-- .header__shop -->
 </header>
-<div class="wrapper">
-    <div class="slider">
+<main class="wrapper">
+    <section class="container-slider">
+        <div class="slider" id="slider">
+            <div class="slider__section">
+                <img src="resources/slider/slide1.jpg" alt="" class="slider__img">
+                <div class="slider__content">
+                    <h2 class="slider__title">TITULO PARA LA PRIMERA SLIDE</h2>
+                    <a href="#" class="button">Comprar</a>
+                </div>
+            </div>
+            <div class="slider__section">
+                <img src="resources/slider/slide2.jpg" alt="" class="slider__img">
+                <div class="slider__content">
+                    <h2 class="slider__title">TITULO PARA LA SEGUNDA SLIDE</h2>
+                    <a href="#" class="button">Comprar</a>
+                </div>
+            </div>
+            <div class="slider__section">
+                <img src="resources/slider/slide3.jpg" alt="" class="slider__img">
+                <div class="slider__content">
+                    <h2 class="slider__title">TITULO PARA LA TERCERA SLIDE</h2>
+                    <a href="#" class="button">Comprar</a>
+                </div>
+            </div>
+            <div class="slider__section">
+                <img src="resources/slider/slide4.jpg" alt="" class="slider__img">
+                <div class="slider__content">
+                    <h2 class="slider__title">TITULO PARA LA CUARTA SLIDE</h2>
+                    <a href="#" class="button">Comprar</a>
+                </div>
+            </div>
+            <div class="slider__button slider__button--right" id="btn-right">&#62</div>
+            <div class="slider__button slider__button--left" id="btn-left">&#60</div>
+        </div>
+        
         <!-- TODO: Desarrollar el marcado para el slider -->
-    </div><!-- .slider -->
-    <div class="products">
+    </section><!-- .slider -->
+    <section class="products">
+        <div class="container">
+            <h2 class="products-title">PRODUCTOS</h2>
+            <article class="container-products">
+                <div class="product">
+                    <img src="resources/products/vans1.png" alt="" class="product__img">
+                    <div class="product__description">
+                        <h3 class="product__title">OLD SKOOL 36 DX</h3>
+                        <span class="product__price">85,00 €</span>
+                    </div>
+                    <span class="button">Comprar</span>
+                </div>
+                <div class="product">
+                    <img src="resources/products/vans2.png" alt="" class="product__img">
+                    <div class="product__description">
+                        <h3 class="product__title">COMFYCUSH OLD SKOOL</h3>
+                        <span class="product__price">75,00 €</span>
+                    </div>
+                    <span class="button">Comprar</span>
+                </div>
+                <div class="product">
+                    <img src="resources/products/vans3.png" alt="" class="product__img">
+                    <div class="product__description">
+                        <h3 class="product__title">OLD SKOOL DE ANTE Y LONA</h3>
+                        <span class="product__price">60,00 €</span>
+                    </div>
+                    <span class="button">Comprar</span>
+                </div>
+                <div class="product">
+                    <img src="resources/products/vans4.png" alt="" class="product__img">
+                    <div class="product__description">
+                        <h3 class="product__title">OLD SKOOL DE ANTE SUAVE</h3>
+                        <span class="product__price">65,00 €</span>
+                    </div>
+                    <span class="button">Comprar</span>
+                </div>
+                <div class="product--double">
+                    <img src="resources/cta/cta1.jpg" alt="" class="product__img--double">
+                </div>
+                <div class="product">
+                    <img src="resources/products/vans5.png" alt="" class="product__img">
+                    <div class="product__description">
+                        <h3 class="product__title">OLD SKOOL PRO</h3>
+                        <span class="product__price">95,00 €</span>
+                    </div>
+                    <span class="button">Comprar</span>
+                </div>
+                <div class="product">
+                    <img src="resources/products/vans6.png" alt="" class="product__img">
+                    <div class="product__description">
+                        <h3 class="product__title">OLD SKOOL PRO</h3>
+                        <span class="product__price">95,00 €</span>
+                    </div>
+                    <span class="button">Comprar</span>
+                </div>
+                <div class="product">
+                    <img src="resources/products/vans7.png" alt="" class="product__img">
+                    <div class="product__description">
+                        <h3 class="product__title">ERA PRIMARY CHECK</h3>
+                        <span class="product__price">95,00 €</span>
+                    </div>
+                    <span class="button">Comprar</span>
+                </div>
+                <div class="product--double">
+                    <img src="resources/cta/cta2.jpg" alt="" class="product__img--double">
+                </div>
+                <div class="product">
+                    <img src="resources/products/vans8.png" alt="" class="product__img">
+                    <div class="product__description">
+                        <h3 class="product__title">STYLE 36</h3>
+                        <span class="product__price">80,00 €</span>
+                    </div>
+                    <span class="button">Comprar</span>
+                </div>
+            </article>
+        </div>
         <!-- TODO: Desarrollar el marcado para los productos -->
-    </div><!-- .products -->
+    </section><!-- .products -->
     <div class="load">
         <!-- TODO: BONUS. Desarrollar la funcionalidad para que el botón cargue más productos -->
         <span class="load__button button">Cargar más productos</span>
     </div><!-- .load -->
-</div><!-- .wrapper -->
+</main><!-- .wrapper -->
 <footer class="footer">
     <p><img src="./resources/logo.png" alt="Neoshop" title="Neoshop" /></p>
     <p>Prueba técnica para candidatos</p>
--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,78 @@
  * TODO: Desarrollar el código del slider
  */
 
+// let slide = document.querySelector('#slider');
+
+// function getData() {
+//      fetch('./api/slides.json')
+//      .then(res => res.json())
+//      .then(info => LoadData(info.data))
+// }
+ 
+// async function LoadData(datos) {
+    
+//      for await( let value of datos){
+//         slide.innerHTML += `
+ 
+//         <div class="slider__section">
+//         <img src="${value.bg_image}" alt="" class="slider__img">
+//         <div class="slider__content">
+//             <h2 class="slider__title">${value.title}</h2>
+//             <a href="${value.button_link}" class="slider__btn button">${value.button_text}</a>
+//         </div>
+//         </div>  
+ 
+//         `
+//     }
+// }
+ 
+// getData();
+
+const slider = document.querySelector('#slider');
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length-1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterBegin', sliderSectionLast)
+
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst)
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+function Prev() {
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length-1];
+    slider.style.marginLeft = "0%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener('click', function() {
+    Next();
+});
+
+btnLeft.addEventListener('click', function() {
+    Prev();
+});
+
+setInterval(function(){
+    Next();
+}, 5000 );
+
 /**
  * TODO: Desarrollar el código para cargar productos por Ajax
  */
+ 
